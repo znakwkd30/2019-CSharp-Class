bab8410667ab2c4cb26da75412825f6861cb53c6
HyeonJun <41042989+znakwkd30@users.noreply.github.com>
2019-09-24 11:03:14 +0900

    [Update]

--- 2019CSharp/2019CSharp/App.xaml.cs
+++ 2019CSharp/2019CSharp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,7 @@ namespace _2019CSharp
     /// </summary>
     public partial class App : Application
     {
+        public static FoodDataSource FoodData = new FoodDataSource();
+        public static Seat seat = new Seat();
     }
 }
--- 2019CSharp/2019CSharp/Properties/Resources.Designer.cs
+++ 2019CSharp/2019CSharp/Properties/Resources.Designer.cs
@@ -52,8 +52,8 @@ namespace _2019CSharp.Properties
         }
 
         /// <summary>
-        ///   이 강력한 형식의 리소스 클래스를 사용하여 모든 리소스 조회에 대해
-        ///   현재 스레드의 CurrentUICulture 속성을 재정의합니다.
+        ///   이 강력한 형식의 리소스 클래스를 사용하여 모든 리소스 조회에 대해 현재 스레드의 CurrentUICulture 속성을
+        ///   재정의합니다.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Globalization.CultureInfo Culture
